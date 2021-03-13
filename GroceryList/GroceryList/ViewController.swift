5177f2b7e900fdedc00338ccb027f732947e34c4
ugurcandemiir <ugurcandemiir@gmail.com>
2021-03-12 20:18:43 -0500

    Added AddButton functionality and Tableview

--- GroceryList/GroceryList/ViewController.swift
+++ GroceryList/GroceryList/ViewController.swift
@@ -1,19 +1,53 @@
 //
 //  ViewController.swift
-//  GroceryList
+//  AddDeleteCell
 //
-//  Created by Ugurcan Demir on 3/12/21.
+//  Created by SHUBHAM AGARWAL on 14/09/19.
+//  Copyright © 2019 SHUBHAM AGARWAL. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var btnAdd: UIButton!
+    @IBOutlet weak var txtInput: UITextField!
+    @IBOutlet weak var tableView: UITableView!
+    
+    var stringArr = [String]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        btnAdd.layer.cornerRadius = 5
+
     }
 
+    @IBAction func onClickAddButton(_ sender: Any) {
+        if let txt = txtInput.text, !txt.isEmpty {
+            self.stringArr.insert(txt, at: 0)
+            tableView.beginUpdates()
+            tableView.insertRows(at: [IndexPath(row: 0, section: 0)], with: .right)
+            tableView.endUpdates()
+            
+            txtInput.text = nil
+        }
+    }
+    
+}
 
+extension ViewController: UITableViewDelegate, UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return stringArr.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "EditTableViewCell", for: indexPath) as? EditTableViewCell else {return UITableViewCell()}
+        cell.lblName.text = stringArr[indexPath.row]
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 70
+    }
 }
 
